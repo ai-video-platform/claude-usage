fc4df8018458c42b382afeb21f798be9dbde9fde
Admin <AI Video Platform>
2026-06-13 08:05:54 -0700

    Initial Commit

---
+++ Claude Usage/Claude_UsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Claude_UsageApp.swift
+//  Claude Usage
+//
+//  Created by Panda on 13/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Claude_UsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
